d0280d18c3545f2d15ff974357a013a0994c5228
周澍 周澍 <zhoushu@up-tech.cn>
2019-09-16 15:49:46 +0800

    792 多线程理财划转至币币失败原因确认

--- service/src/main/java/cn/idachain/finance/batch/service/external/CexResponse.java
+++ service/src/main/java/cn/idachain/finance/batch/service/external/CexResponse.java
@@ -1,13 +1,17 @@
 package cn.idachain.finance.batch.service.external;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by liuhailin on 2019/2/2.
  */
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CexResponse {
     private String code;
     private Object data;
